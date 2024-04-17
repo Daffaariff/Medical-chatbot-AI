ab46768821562fd50da0e8c5e1fc9ac8c04623dd
Bistowsky <dafa.130.df@gmail.com>
2024-04-17 22:49:50 +0700

    Upload:APP Ready to Deploy

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Daffa Arifadilah',
+    author_email= 'dafa.130.df@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
